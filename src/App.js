f45105637c5894ac721a2187fefbdb93681acd82
jiwanChaudhary <jeewanchaudhary6@gmail.com>
2022-08-22 17:42:11 +0545

    Initial Commit, @mui, @mui/icons-material, favicon and title added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./GlobalStyle.css"
+import { useEffect } from "react"
+import { Typography } from "@mui/material";
 
 function App() {
+  useEffect(() => {
+    document.title = "Revolve Nepal"
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography variant="h2">Revolve Nepal</Typography>
   );
 }
 
